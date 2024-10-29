606e879371d672084c4b1213d0ae1f7c1b8c22bb
PzDarby <izliquihd@gmail.com>
2024-10-29 09:40:04 +0100

    Añadida las funciones de Par e Impar

--- reto1/src/reto1/Reto1Funciones2.java
+++ reto1/src/reto1/Reto1Funciones2.java
@@ -7,4 +7,14 @@ public class Reto1Funciones2 {
 	public static double  restar(double num1,double num2) {
 		return num1-num2;
 	}
+	public static boolean esPar(double num1) {
+		if(num1%2==0)
+			return true;
+		return false;
+	}
+	public static boolean esImpar(double num1) {
+		if(!(num1%2==0))
+			return true;
+		return false;
+	}
 }
