c5e4f6fe50e16e25030497c544c1e17a46c2e506
PzDarby <izliquihd@gmail.com>
2024-10-29 09:59:51 +0100

    Merge branch 'main' of https://github.com/alvarezleiva/reto1.git into main

--- reto1/src/reto1/Reto1.java
+++ reto1/src/reto1/Reto1.java
@@ -1,9 +1,16 @@
 package reto1;
 
+import java.util.Scanner;
+import reto1.Reto1Funciones;
+import reto1.Reto1Funciones2;
 public class Reto1 {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		int num1=0;
+		do {
+			System.out.println("");
+		}while(!(num1>=0 && num1<9));
 	}
 
 }
