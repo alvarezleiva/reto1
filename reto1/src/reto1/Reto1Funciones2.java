a576e6d5a815b131104478196c3a0876cba5c62e
PzDarby <izliquihd@gmail.com>
2024-10-29 09:34:12 +0100

    Funciones restar y sumar

--- reto1/src/reto1/Reto1Funciones2.java
+++ reto1/src/reto1/Reto1Funciones2.java
@@ -1,5 +1,10 @@
 package reto1;
 
 public class Reto1Funciones2 {
-
+	public static double sumar(double num1,double num2) {
+		return num1+num2;
+	}
+	public static double  restar(double num1,double num2) {
+		return num1-num2;
+	}
 }
