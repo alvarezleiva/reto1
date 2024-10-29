45e936a3e43ef6b306743a8d7f55816997336801
PzDarby <izliquihd@gmail.com>
2024-10-29 10:17:00 +0100

    Añaido el menu + validaciones

--- reto1/src/reto1/Reto1.java
+++ reto1/src/reto1/Reto1.java
@@ -9,7 +9,29 @@ public class Reto1 {
 		Scanner sc = new Scanner(System.in);
 		int num1=0;
 		do {
-			System.out.println("");
+			//Funcion imprimir para poner lineas por consola
+			Reto1Funciones2.imprimir("-", 100);
+			System.out.println("Elige la operacion");
+			Reto1Funciones2.imprimir("-", 100);
+			System.out.println("1. Sumar");
+			System.out.println("2. Restar");
+			System.out.println("3. Multiplicar");
+			System.out.println("4. Dividir");
+			System.out.println("5. Par");
+			System.out.println("6. Impar");
+			System.out.println("7. Positivo");
+			System.out.println("8. Negativo");
+			System.out.println("0. Salir");
+			// Validacion del numero introducido
+			do {
+				try {
+					String s = sc.nextLine();
+					num1 = Integer.parseInt(s);
+					break;
+				} catch (Exception e) {
+					System.out.println("Formato incorrecto");
+				}
+			}while(true);
 		}while(!(num1>=0 && num1<9));
 	}
 
