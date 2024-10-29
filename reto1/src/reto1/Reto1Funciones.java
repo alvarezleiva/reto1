0e2f79dd768c5983c595baac1440a09678f34e6f
alvarezleiva <DAM1@aula11-pc06>
2024-10-29 09:45:31 +0100

    creadas funciones multi, dividir, espositivo y esnegativo

--- reto1/src/reto1/Reto1Funciones.java
+++ reto1/src/reto1/Reto1Funciones.java
@@ -2,6 +2,38 @@ package reto1;
 
 public class Reto1Funciones {
 
+	public static double multiplicar(int a, int b) {
 
+		int multi = a * b;
+		return multi;
+
+	}
+
+	public static double dividir(int a, int b) {
+
+		int divide = a / b;
+		return divide;
+
+	}
+
+	public static boolean esPositivo(double positivo) {
+
+		if (positivo > 0) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public static boolean esNegativo(double negativo) {
+
+		if (negativo < 0) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
 
 }
